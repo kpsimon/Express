c26cb0c12d842d5f35d6e9d7afa6aada77943a76
kpsimon <44358408+kpsimon@users.noreply.github.com>
2020-05-13 00:34:26 -0700

    Update app.js

--- CodeStrips/app.js
+++ CodeStrips/app.js
@@ -30,19 +30,21 @@ app.post('/strips',(req, res, next) => {
   const new_strip = req.body.strip;
   if(new_strip.head && new_strip.body && new_strip.background && new_strip.bubbleType)
   {
-    db.run('INSERT INTO Stripe (head, body, background, bubbleType, bubbleText, caption) VALUES ($head, $body, $background, $bubbleType, $bubbleText, $caption)',{
-    $head: new_strip.head, 
-    $body: new_strip.body, 
-    $background: new_strip.background, 
-    $bubbleType: new_strip.bubbleType, 
-    $bubbleText: new_strip.bubbleText, 
-    $caption: new_strip.caption
-}, function(err) {
-      if(err)
-      {
-        return res.status(500).send();
-      }
-});
+    db.run('INSERT INTO Stripe (head, body, background, bubbleType, bubbleText, caption) VALUES ($head, $body, $background, $bubbleType, $bubbleText, $caption)',
+           {
+              $head: new_strip.head, 
+              $body: new_strip.body, 
+              $background: new_strip.background, 
+              $bubbleType: new_strip.bubbleType, 
+              $bubbleText: new_strip.bubbleText, 
+              $caption: new_strip.caption
+          }, 
+           function(err) {
+              if(err)
+              {
+                return res.status(500).send();
+              }
+      });
     res.send(new_strip);
   }
   else 
